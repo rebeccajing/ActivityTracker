2ad62bb322a8d8829e270d9c1775d75c1efe2df3
M66B <marcel@bokhorst.biz>
2014-04-29 12:07:55 +0200

    Added step counter listener

--- src/biz/bokhorst/am/BackgroundService.java
+++ src/biz/bokhorst/am/BackgroundService.java
@@ -12,69 +12,67 @@ import com.google.android.gms.location.ActivityRecognitionClient;
 import com.google.android.gms.location.ActivityRecognitionResult;
 import com.google.android.gms.location.DetectedActivity;
 
+import android.annotation.TargetApi;
 import android.app.IntentService;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
 public class BackgroundService extends IntentService implements
-		ConnectionCallbacks, OnConnectionFailedListener {
-
+		ConnectionCallbacks, OnConnectionFailedListener, SensorEventListener {
 	private static String TAG = "AM";
-	private static PendingIntent locationPendingIntent = null;
+
 	private static ActivityRecognitionClient activityRecognitionClient = null;
+	private static PendingIntent locationPendingIntent = null;
+	private static boolean stepCounterRegistered = false;
 
 	public static final String ACTION_START = "Start";
 	public static final String ACTION_LOCATION = "Location";
+	public static final String ACTION_STEPS = "Steps";
 
 	public BackgroundService() {
 		super(TAG);
 	}
 
 	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Auto-generated method stub
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	@Override
 	protected void onHandleIntent(Intent intent) {
 		Log.w(TAG, "Handling intent, action=" + intent.getAction());
 
-		// Start activity recognition (again)
+		// Start activity recognition
 		ensureActivityRecognition();
 
-		// Start location updates (again)
+		// Start location updates
 		ensureLocationUpdates();
+
+		// Start step counter
+		ensureStepCounting();
 
 		// Check for activity recognition result
 		if (ActivityRecognitionResult.hasResult(intent))
 			handleActivityRecognition(intent);
+		else if (ACTION_START.equals(intent.getAction()))
+			handleStart(intent);
 		else if (ACTION_LOCATION.equals(intent.getAction()))
 			handleLocationChanged(intent);
-	}
-
-	private void handleActivityRecognition(Intent intent) {
-		ActivityRecognitionResult result = ActivityRecognitionResult
-				.extractResult(intent);
-		DetectedActivity mostProbableActivity = result
-				.getMostProbableActivity();
-		int confidence = mostProbableActivity.getConfidence();
-		int activityType = mostProbableActivity.getType();
-
-		String activityName = getNameFromType(activityType);
-		SimpleDateFormat TIME_FORMATTER = new SimpleDateFormat("HH:mm:ss",
-				Locale.getDefault());
-
-		for (DetectedActivity activity : result.getProbableActivities())
-			Log.w(TAG, TIME_FORMATTER.format(new Date(result.getTime()))
-					+ " Activity " + getNameFromType(activity.getType()) + " "
-					+ activity.getConfidence() + " %");
-	}
-
-	private void handleLocationChanged(Intent intent) {
-		Location location = (Location) intent.getExtras().get(
-				LocationManager.KEY_LOCATION_CHANGED);
-		Log.w(TAG, "Location=" + location);
+		else if (ACTION_STEPS.equals(intent.getAction()))
+			handleStepsChanged(intent);
 	}
 
 	private void ensureActivityRecognition() {
@@ -94,28 +92,6 @@ public class BackgroundService extends IntentService implements
 			}
 		} else
 			Log.w(TAG, "Play services not available, result=" + result);
-	}
-
-	private void ensureLocationUpdates() {
-		if (locationPendingIntent == null) {
-			// Build pending intent
-			Intent locationIntent = new Intent(this, BackgroundService.class);
-			locationIntent.setAction(BackgroundService.ACTION_LOCATION);
-
-			locationPendingIntent = PendingIntent.getService(this, 0,
-					locationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-			// Request location updates
-			Log.w(TAG, "Requesting location updates");
-			LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-			locationManager.removeUpdates(locationPendingIntent);
-			int minTime = 60 * 1000; // TODO: setting
-			int minDistance = 50; // TODO: setting
-			Criteria criteria = new Criteria();
-			criteria.setAccuracy(Criteria.ACCURACY_FINE);
-			locationManager.requestLocationUpdates(minTime, minDistance,
-					criteria, locationPendingIntent);
-		}
 	}
 
 	@Override
@@ -139,6 +115,91 @@ public class BackgroundService extends IntentService implements
 	@Override
 	public void onDisconnected() {
 		Log.w(TAG, "Disconnected from Play services");
+	}
+
+	private void ensureLocationUpdates() {
+		if (locationPendingIntent == null) {
+			// Build pending intent
+			Intent locationIntent = new Intent(this, BackgroundService.class);
+			locationIntent.setAction(BackgroundService.ACTION_LOCATION);
+
+			locationPendingIntent = PendingIntent.getService(this, 0,
+					locationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+			// Request location updates
+			LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+			locationManager.removeUpdates(locationPendingIntent);
+			int minTime = 60 * 1000; // TODO: setting
+			int minDistance = 50; // TODO: setting
+			Criteria criteria = new Criteria();
+			criteria.setAccuracy(Criteria.ACCURACY_FINE);
+			locationManager.requestLocationUpdates(minTime, minDistance,
+					criteria, locationPendingIntent);
+			Log.w(TAG, "Requested location updates");
+		}
+	}
+
+	@TargetApi(Build.VERSION_CODES.KITKAT)
+	private void ensureStepCounting() {
+		if (getPackageManager().hasSystemFeature(
+				PackageManager.FEATURE_SENSOR_STEP_COUNTER)) {
+			if (!stepCounterRegistered) {
+				SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+				Sensor countSensor = sensorManager
+						.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+				sensorManager.registerListener(this, countSensor,
+						SensorManager.SENSOR_DELAY_NORMAL);
+				stepCounterRegistered = true;
+				Log.w(TAG, "Step counter listener registered");
+			}
+		} else
+			Log.w(TAG, "No hardware step counter");
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		Log.w(TAG, "Accuracy changed, accuracy=" + accuracy);
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		int steps = (int) event.values[0];
+		Log.w(TAG, "Sensor changed, steps=" + steps);
+		Intent intentSteps = new Intent(this, BackgroundService.class);
+		intentSteps.setAction(ACTION_STEPS);
+		intentSteps.putExtra(ACTION_STEPS, steps);
+		startService(intentSteps);
+	}
+
+	private void handleStart(Intent intent) {
+	}
+
+	private void handleActivityRecognition(Intent intent) {
+		ActivityRecognitionResult result = ActivityRecognitionResult
+				.extractResult(intent);
+		DetectedActivity mostProbableActivity = result
+				.getMostProbableActivity();
+		int confidence = mostProbableActivity.getConfidence();
+		int activityType = mostProbableActivity.getType();
+		String activityName = getNameFromType(activityType);
+
+		SimpleDateFormat TIME_FORMATTER = new SimpleDateFormat("HH:mm:ss",
+				Locale.getDefault());
+		for (DetectedActivity activity : result.getProbableActivities())
+			Log.w(TAG, TIME_FORMATTER.format(new Date(result.getTime()))
+					+ " Activity " + getNameFromType(activity.getType()) + " "
+					+ activity.getConfidence() + " %");
+	}
+
+	private void handleLocationChanged(Intent intent) {
+		Location location = (Location) intent.getExtras().get(
+				LocationManager.KEY_LOCATION_CHANGED);
+		Log.w(TAG, "Location=" + location);
+	}
+
+	private void handleStepsChanged(Intent intent) {
+		int steps = intent.getIntExtra(ACTION_STEPS, -1);
+		Log.w(TAG, "Steps=" + steps);
 	}
 
 	// Helper methods
