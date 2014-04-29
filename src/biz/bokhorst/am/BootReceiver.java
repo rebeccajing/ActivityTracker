bb896c52421d33089954906a95412498f33e2ae3
M66B <marcel@bokhorst.biz>
2014-04-29 10:52:45 +0200

    Handle app update

--- src/biz/bokhorst/am/BootReceiver.java
+++ src/biz/bokhorst/am/BootReceiver.java
@@ -3,11 +3,13 @@ package biz.bokhorst.am;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class BootReceiver extends BroadcastReceiver {
 
 	@Override
-	public void onReceive(final Context context, Intent bootIntent) {
+	public void onReceive(final Context context, final Intent intent) {
+		Log.w("AM", "Boot receiver, action=" + intent.getAction());
 		Intent initService = new Intent(context, BackgroundService.class);
 		initService.setAction(BackgroundService.ACTION_START);
 		context.startService(initService);
